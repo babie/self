8a75c5b15068b514ea04ef51c3711c58c1a9a9da
babie <babie.wildheart@gmail.com>
2022-01-25 20:43:17 +0900

    feat: try splat-routes

---
+++ app/routes/$.tsx
@@ -0,0 +1,24 @@
+import { useParams } from 'remix'
+import type { LoaderFunction, ActionFunction } from 'remix'
+
+export const loader: LoaderFunction = async ({ params }) => {
+  console.log(params['*'])
+  return {
+    nothing: 'none',
+  }
+}
+
+export const action: ActionFunction = async ({ params }) => {
+  console.log(params['*'])
+}
+
+export default function PostRoute() {
+  const params = useParams()
+  console.log(params['*'])
+  return (
+    <>
+      <h1>/$.tsx</h1>
+      {params['*']}
+    </>
+  )
+}
